e5c84492d2c59d4e8ff22ac51677cbc2518b9399
plastikaweb <info@plastikaweb.com>
2021-03-16 15:46:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-fib-dynamic-programming
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-fib-dynamic-programming)
---
+++ index.js
@@ -0,0 +1,15 @@
+// memoization fibonnaci number
+// js object, keys will be arg to fn, value will the return value
+
+const f = (n, memo = {}) => {
+  if (n in memo) return memo[n];
+  if (n <= 2) return 1;
+  memo[n] = f(n - 1, memo) + f(n - 2, memo);
+  console.log(memo);
+  return memo[n];
+};
+
+console.log(f(6));
+console.log(f(7));
+console.log(f(8));
+console.log(f(50));
